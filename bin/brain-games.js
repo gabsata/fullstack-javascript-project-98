7afa26d46aecbfe0585a41b54c14d0b7d8343604
Gabriel Caceres <gabrielcaceres1995@gmail.com>
2026-02-25 10:26:36 -0500

    bienvenida al usuario

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,4 @@
 console.log("¡Bienvenido a Brain Games!")
+import readlineSync from 'readline-sync';
+const unsername=readlineSync.question("Cual es tu nombre?");
+console.log(`¡Hola, ${unsername}!`)
