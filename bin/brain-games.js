f20b7d5e173bbd42eef4d472d51793c0b7caffd9
Gabriel Caceres <gabrielcaceres1995@gmail.com>
2026-03-06 13:30:49 -0500

    error resuelto

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
-console.log("¡Bienvenido a Brain Games!")
 import readlineSync from 'readline-sync';
+console.log("¡Bienvenido a Brain Games!")
 const unsername=readlineSync.question("Cual es tu nombre?");
 console.log(`¡Hola, ${unsername}!`)
 
