27c24b1f0b6356b6a207e1fe612f50358bfdf6ae
Gabriel Caceres <gabrielcaceres1995@gmail.com>
2026-03-09 10:20:21 -0500

    aleatorio

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
-console.log("¡Bienvenido a Brain Games!"),
-const username = readlineSync.question("Cual es tu nombre?");
+
+console.log('¡Bienvenido a Brain Games!');
+const username = readlineSync.question('Cual es tu nombre?');
 console.log(`¡Hola, ${username}!`);
 console.log('Responde "yes" si el número es par, de lo contrario responde "no".');
-const number = Math.floor(Math.random() * 100)
+const number = Math.floor(Math.random() * 100);
 console.log(`Pregunta: ${number}`);
 const answer = readlineSync.question('Tu respuesta: ');
 const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
@@ -12,4 +13,3 @@ if (answer === correctAnswer) {
 } else {
   console.log('Respuesta incorrecta.');
 }
-
