8165e538c5327d7cb0d2983245bebddf8e446ad2
Gabriel Caceres <gabrielcaceres1995@gmail.com>
2026-03-09 10:04:13 -0500

    paso 4 número aleatorio

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
+[![Bugs](https://sonarcloud.io/api/project_badges/measure?project=<PROJECT_KEY>&metric=bugs)](https://sonarcloud.io/summary/new_code?id=<PROJECT_KEY>)
+[![Code Smells](https://sonarcloud.io/api/project_badges/measure?project=<PROJECT_KEY>&metric=code_smells)](https://sonarcloud.io/summary/new_code?id=<PROJECT_KEY>)
 [![Actions Status](https://github.com/gabsata/fullstack-javascript-project-98/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/gabsata/fullstack-javascript-project-98/actions)
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,15 @@
 import readlineSync from 'readline-sync';
-console.log("¡Bienvenido a Brain Games!")
-const unsername=readlineSync.question("Cual es tu nombre?");
-console.log(`¡Hola, ${unsername}!`)
+console.log("¡Bienvenido a Brain Games!"),
+const username = readlineSync.question("Cual es tu nombre?");
+console.log(`¡Hola, ${username}!`);
+console.log('Responde "yes" si el número es par, de lo contrario responde "no".');
+const number = Math.floor(Math.random() * 100)
+console.log(`Pregunta: ${number}`);
+const answer = readlineSync.question('Tu respuesta: ');
+const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+if (answer === correctAnswer) {
+  console.log('¡Correcto!');
+} else {
+  console.log('Respuesta incorrecta.');
+}
 
