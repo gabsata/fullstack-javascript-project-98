e0629991dafb49af5059e82a83ecc39cd24d565e
Gabriel Caceres <gabrielcaceres1995@gmail.com>
2026-03-30 10:05:18 -0500

    se agrega comando de calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+console.log('¡Bienvenido a Brain Games!');
+const username = readlineSync.question('Cual es tu nombre?');
+console.log(`¡Hola, ${username}!`);
+
+console.log('Cual es el resultado de esta operación?')
+
+
