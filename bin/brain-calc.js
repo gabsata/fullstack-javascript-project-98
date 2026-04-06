ee7e9539b726c3a1ac2da23b194313358aabfa74
Gabriel Caceres <gabrielcaceres1995@gmail.com>
2026-04-06 14:06:59 -0500

    se gregan las lineas de calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,8 +1,29 @@
 import readlineSync from 'readline-sync';
 console.log('¡Bienvenido a Brain Games!');
-const username = readlineSync.question('Cual es tu nombre?');
-console.log(`¡Hola, ${username}!`);
+const name = readlineSync.question('Cual es tu nombre?');
+console.log(`¡Hola, ${name}!`);
+console.log('Cual es el resultado de esta operación?');
+function generarOperacion(){
+    const operaciones = ['+', '-' '*', '/'];
+    const num1 = Math.floor(Math.random() * 10) + 1;
+    const num2 = Math.floor(Math.random() * 10) + 1;
 
-console.log('Cual es el resultado de esta operación?')
+  const operacion = operaciones[Math.floor(Math.random() * operaciones.length)];
+   let resultado;
 
+  switch (operacion) {
+    case '+':
+      resultado = num1 + num2;
+      break;
+    case '-':
+      resultado = num1 - num2;
+      break;
+    case '*':
+      resultado = num1 * num2;
+      break;
+    case '/':
+     resultado = num1 / num2;
+  }
 
+  return `${num1} ${operacion} ${num2}`;
+}
