e965f685651d742628c94dd526b160816cfc2a68
gabsata <gabrielcaceres1995@gmail.com>
2026-04-07 19:01:53 -0500

    corrección del código par ala calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,29 +1,41 @@
 import readlineSync from 'readline-sync';
-console.log('¡Bienvenido a Brain Games!');
-const name = readlineSync.question('Cual es tu nombre?');
-console.log(`¡Hola, ${name}!`);
-console.log('Cual es el resultado de esta operación?');
-function generarOperacion(){
-    const operaciones = ['+', '-' '*', '/'];
-    const num1 = Math.floor(Math.random() * 10) + 1;
-    const num2 = Math.floor(Math.random() * 10) + 1;
 
+function getRandomOperator() {
+  const operaciones = ['+', '-', '*', '/'];
   const operacion = operaciones[Math.floor(Math.random() * operaciones.length)];
-   let resultado;
-
+  return operacion;
+}
+function generarOperacion(num1, num2, operacion) {
   switch (operacion) {
     case '+':
-      resultado = num1 + num2;
-      break;
-    case '-':
-      resultado = num1 - num2;
-      break;
-    case '*':
-      resultado = num1 * num2;
-      break;
-    case '/':
-     resultado = num1 / num2;
-  }
+      return num1 + num2;
 
-  return `${num1} ${operacion} ${num2}`;
+    case '-':
+      return num1 - num2;
+
+    case '*':
+      return num1 * num2;
+
+    case '/':
+      return num1 / num2;
+
+    default:
+      return num1 / num2;
+  }
 }
+const getRound = function () {
+  console.log('¡Bienvenido a Brain Games!');
+  const name = readlineSync.question('Cual es tu nombre?');
+  console.log(`¡Hola, ${name}!`);
+  for (let i = 0; i < 3; i += 1) {
+    const randomOperator = getRandomOperator();
+    const num1 = Math.floor(Math.random() * 10) + 1;
+    const num2 = Math.floor(Math.random() * 10) + 1;
+    console.log(`${num1} ${randomOperator} ${num2}Cual es el resultado de esta operación?`);
+    const resultado = generarOperacion(num1, num2, randomOperator);
+  }
+};
+getRound()
+//Tengo que pedir la respuesta del usuario, respuesta, comparar la respuesta del usuario con el rsultado
+//fijarse si el resultado para que la comparación funcione
+//si las respuestas son diferente debe salirse del bucle
